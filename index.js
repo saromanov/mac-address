bdbeede5422fd98ca623e67bab195f291d98abc6
Sergey <xxsmotur@gmail.com>
2019-08-11 18:33:09 +0500

    fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-exec("date | md5sum | sed -r 's/(..){3}/\1:/g;s/\s+-$//'", (err, stdout) => {
+exec("date | md5sum | sed -r 's/(..){3}/\\1:/g;s/\\s+-$//'", (err, stdout) => {
   if (err) {
     return;
   }
