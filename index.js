ba4b2c7c2980a86356a50a803c68501d38734e83
Sergey <xxsmotur@gmail.com>
2019-08-11 18:31:02 +0500

    inif of the package

---
+++ index.js
@@ -0,0 +1,8 @@
+const { exec } = require('child_process');
+exec("date | md5sum | sed -r 's/(..){3}/\1:/g;s/\s+-$//'", (err, stdout) => {
+  if (err) {
+    return;
+  }
+
+  console.log(stdout);
+});
